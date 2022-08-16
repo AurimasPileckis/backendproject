5e9215c77ef19d862741f3ba29aa291ddfbaed36
AurimasPileckis <aurimas.pileckis@gmail.com>
2022-08-16 11:03:35 +0300

    bit.aurimas

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const database = await mysql.createConnection({
     database: 'aurimasp'
 })
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.get('/', async (req, res) => {
     const songs = await database.query('SELECT id, song_Name, song_Album FROM songs')
